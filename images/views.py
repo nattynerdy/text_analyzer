d6402ec60cfd527649d00097e4d401f374260ed0
Natalie Euley <natalie.euley@galvanize.com>
2024-01-02 19:44:09 -0600

    Starting on image upload infrastructure

--- images/views.py
+++ images/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from forms import ImageForm
 
-def analyze_image(request):
+def analyze_image(request:HttpRequest):
+    if request.method == "POST":
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
     return render(request, "")
 
 def all_images(request):
